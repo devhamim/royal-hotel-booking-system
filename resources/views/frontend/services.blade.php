b72b6a9d45bba846c49ca5e4f14f94e90f87250b
devhamim <hamim735@gmail.com>
2023-12-31 20:16:58 +0600

    service

--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -37,7 +37,7 @@
 			<div class="col-lg-6 col-md-12">
 				<div class="section-title two" data-cue="zoomIn">
 					<h4>Facilities</h4>
-					<h2>RESOTE’S EXTRA FACILITIES FOR LUXURIOUS LIFE</h2>
+					<h2>The Full Moon Canada for enjoy your luxurious life</h2>
 				</div>
 			</div>
 		</div>
@@ -70,7 +70,7 @@
 					<div class="facilities-content">
 						<h4>Swimming</h4>
 					    <h2>Indoor Swimming Pool</h2>
-						<p>Discover relaxation in our luxury hotel's indoor swimming pool. Encased in sleek design and ambient lighting, the pool offers a tranquil retreat. Immerse yourself in its inviting waters and enjoy a serene escape with impeccable service in a refined setting.</p>
+						<p>Enjoy a relaxing swim in our indoor pool at the Canadian hotel. The pool is comfortably heated, and our inviting surroundings make it the perfect place to unwind. Whether you're looking for a refreshing dip or a leisurely swim, our indoor pool is here for your enjoyment. So, grab your swimsuit, and take a dip to enhance your stay with us!</p>
 						{{-- <a class="facilities-button" href="services-details.html"><i class="bi bi-arrow-right"></i></a> --}}
 					</div>
 					<div class="facilities-number two" data-cue="zoomIn">
@@ -101,7 +101,7 @@
 					<div class="facilities-content">
 						<h4>Restaurent</h4>
 					    <h2>The Restaurent Center</h2>
-						<p>Nestled within our luxury hotel in Canada, the Restaurant Center offers a culinary journey blending local flavors and global cuisine. Immerse in elegant ambiance, exceptional service, and a diverse, exquisite menu.</p>
+						<p>Savor exquisite dining at our Canadian hotel and resort. Our restaurant offers a diverse menu, from hearty breakfasts to gourmet dinners. With a cozy ambiance and attentive staff, it's the perfect spot to enjoy delicious meals. Whether you're here for a leisurely brunch or a romantic dinner, our restaurant provides a delightful culinary experience for a memorable stay.p>
 						{{-- <a class="facilities-button" href="services-details.html"><i class="bi bi-arrow-right"></i></a> --}}
 					</div>
 					<div class="facilities-number">
@@ -116,7 +116,7 @@
 					<div class="facilities-content">
 						<h4>SPA</h4>
 					    <h2>SPA and Parlor Center</h2>
-						<p>Experience unparalleled tranquility at our luxury hotel's Spa and Parlor Center in Canada. Indulge in rejuvenating treatments, personalized wellness services, and opulent relaxation, curated to enhance your overall sense of well-being.</p>
+						<p>Full Moon Canada's luxury spa with calming massages and rejuvenating facials. Our spa features serene spaces, expert therapists, and a peaceful atmosphere. Treat yourself to a day of relaxation – you'll leave feeling refreshed and revitalized, ready to make the most of your stay with us.</p>
 						{{-- <a class="facilities-button" href="services-details.html"><i class="bi bi-arrow-right"></i></a> --}}
 					</div>
 					<div class="facilities-number two">
