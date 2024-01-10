0fa5275f1e7b8c1146b4f3b3e492361afa383c5a
devhamim <hamim735@gmail.com>
2024-01-10 13:43:26 +0600

    map

--- resources/views/frontend/contect.blade.php
+++ resources/views/frontend/contect.blade.php
@@ -116,15 +116,15 @@
 <!--==================================================-->
 <!-- Start Royella Google Map Area -->
 <!--==================================================-->
-<div class="google-map">
+{{-- <div class="google-map">
 	<div class="row">
 		<div class="col-md-12">
 			<div class="google-map-content">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.0662009102393!2d-73.96815832342757!3d40.78255873329212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2589a018531e3%3A0xb9df1f7387a94119!2sCentral%20Park!5e0!3m2!1sen!2sbd!4v1697123394994!5m2!1sen!2sbd"></iframe>
+				
 			</div>
 		</div>
 	</div>
-</div>
+</div> --}}
 <!--==================================================-->
 <!-- End Royella Google Map Area -->
 <!--==================================================-->
